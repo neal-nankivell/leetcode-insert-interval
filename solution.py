492aeceb8258de73199144432a2fe9f86f53ee9d
neal.nankivell <neal.nankivell@gmail.com>
2019-09-06 14:18:33 +0200

    Implmented sliding window solution
    There are two main paths to this,
    first we find the first and last overlapping window.
    merge these with the new interval and insert this into the middle of
    result skipping over all overlapping windows
    
    if there are no overlapping windows we just need to insert
    the interval into the correct position.

--- solution.py
+++ solution.py
@@ -1,3 +1,33 @@
+from typing import List
+
+
 class Solution:
-    def foo(self) -> str:
-        return "bar"
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [newInterval]
+        i = 0
+        j = len(intervals) - 1
+
+        while i < j and not self.overlap(intervals[i], newInterval):
+            i += 1
+
+        while i <= j and not self.overlap(intervals[j], newInterval):
+            j -= 1
+
+        if (self.overlap(intervals[i], newInterval)):
+            newInterval = self.merge(self.merge(
+                intervals[i], newInterval), intervals[j])
+            return intervals[:i] + [newInterval] + intervals[j + 1:]
+
+        i = 0
+
+        while i <= len(intervals) - 1 and intervals[i][0] < newInterval[0]:
+            i += 1
+
+        return intervals[:i] + [newInterval] + intervals[i:]
+
+    def overlap(self, a: List[int], b: List[int]) -> bool:
+        return a[0] <= b[1] and b[0] <= a[1]
+
+    def merge(self, a: List[int], b: List[int]) -> List[int]:
+        return [min(a[0], b[0]), max(a[1], b[1])]
--- test_solution.py
+++ test_solution.py
@@ -6,8 +6,30 @@ class Test_TestSolution(unittest.TestCase):
     def setUp(self):
         self.sut = solution.Solution()
 
-    def test_foo(self):
-        self.assertEqual(self.sut.foo(), "bar")
+    def test_insert_new(self):
+        self.assertEqual(
+            self.sut.insert([[1, 2]], [4, 5]),
+            [[1, 2], [4, 5]])
+
+    def test_insert_overlapping_one(self):
+        self.assertEqual(
+            self.sut.insert([[1, 2]], [2, 5]),
+            [[1, 5]])
+
+    def test_insert_overlapping_two(self):
+        self.assertEqual(
+            self.sut.insert([[1, 2], [3, 4]], [2, 5]),
+            [[1, 5]])
+
+    def test_insert_into_empty(self):
+        self.assertEqual(
+            self.sut.insert([], [2, 5]),
+            [[2, 5]])
+
+    def test_insert_at_start(self):
+        self.assertEqual(
+            self.sut.insert([[2, 5]], [0, 0]),
+            [[0, 0], [2, 5]])
 
 
 if __name__ == '__main__':
